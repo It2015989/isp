494f25fb627c199bcf176459f6d5fa9b3409c43c
It2015989 <115916034+It2015989@users.noreply.github.com>
2022-10-16 12:17:42 +0530

    Update Screenshot.py

--- Screenshot.py
+++ Screenshot.py
@@ -13,9 +13,9 @@ def take_screenshots():  # Running the while loop for infinite time
         monitor = mss_instance.monitors[0]
         screenshot = mss_instance.grab(monitor)
 
-        file_name = time.strftime("%Y%m%d-%H%M%S")+".png"
+        file_name = time.strftime("%Y%m%d-%H%M%S")+".png" #cerating file name
         # Convert to PIL.Image
         img = Image.frombytes("RGB", screenshot.size,
                               screenshot.bgra, "raw", "BGRX")
         img.save(
-            f"C:\\1)SLIIT\\3rd Year\\2nd Semester\\Information Security Project\\Tests\\Test 2\\Screenshots\\{file_name}")
+            f"C:\\1)SLIIT\\3rd Year\\2nd Semester\\Information Security Project\\Tests\\Test 2\\Screenshots\\{file_name}") #save capture screenshot
